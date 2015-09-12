89b973579cfc7e4c95dab2f88655eb33ddd8edef
John Chang <tschangc@gmail.com>
2015-09-12 20:33:53 +0800

    fix conflicts

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,16 +1,7 @@
 class Issue < ActiveRecord::Base
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-  has_many :votes
-  has_many :vote_users, through: :votes, source: :user, :dependent => :destroy
-=======
-  has_many :votes
-  has_many :users, :through => :votes
->>>>>>> b4f7665bba236b9f6044cf55c6c6f9be098a060d
 
-=======
   has_many :votes, :dependent => :destroy
-  has_many :users, :through => :votes
->>>>>>> 8f6b277cf09c40e2e933cf21159514f1a526df38
+  has_many :vote_users, through: :votes, source: :user, :dependent => :destroy
+
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,10 +1,6 @@
 class Vote < ActiveRecord::Base
-<<<<<<< HEAD
 
-  belongs_to :user, :dependent => :destroy;
-  belongs_to :issue, :counter_cache => true, :dependent => :destroy;
-=======
   belongs_to :user
-  belongs_to :issue
->>>>>>> 8f6b277cf09c40e2e933cf21159514f1a526df38
+  belongs_to :issue, :counter_cache => true
+
 end
