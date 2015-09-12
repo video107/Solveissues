3f508adf653ffbd9cb180c17a36a3026cb686d1b
John Chang <tschangc@gmail.com>
2015-09-12 17:35:24 +0800

    1. remove scafolld scss 2. add more columns to user

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :des_user_session
   end
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
