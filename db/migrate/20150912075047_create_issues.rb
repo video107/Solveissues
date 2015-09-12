63919b1cc370155aecd8b180e3503a31cc6a7762
王信凱 <jcskyting@gmail.com>
2015-09-12 16:02:22 +0800

    scaffold issues

--- db/migrate/20150912075047_create_issues.rb
+++ db/migrate/20150912075047_create_issues.rb
@@ -1,6 +1,9 @@
 class CreateIssues < ActiveRecord::Migration
   def change
     create_table :issues do |t|
+      t.string :title
+      t.text :description
+      t.integer :creator
 
       t.timestamps null: false
     end
