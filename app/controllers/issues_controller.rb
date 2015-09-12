8980441bb3e2af7e1c95c8ddc2a54192bbb3977c
王信凱 <jcskyting@gmail.com>
2015-09-12 16:05:10 +0800

    remove issue edit / delete

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -39,27 +39,27 @@ class IssuesController < ApplicationController
 
   # PATCH/PUT /issues/1
   # PATCH/PUT /issues/1.json
-  def update
-    respond_to do |format|
-      if @issue.update(issue_params)
-        format.html { redirect_to @issue, notice: 'Issue was successfully updated.' }
-        format.json { render :show, status: :ok, location: @issue }
-      else
-        format.html { render :edit }
-        format.json { render json: @issue.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @issue.update(issue_params)
+  #       format.html { redirect_to @issue, notice: 'Issue was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @issue }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @issue.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /issues/1
   # DELETE /issues/1.json
-  def destroy
-    @issue.destroy
-    respond_to do |format|
-      format.html { redirect_to issues_url, notice: 'Issue was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @issue.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to issues_url, notice: 'Issue was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
