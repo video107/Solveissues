e154fcdf4ed76ca0b2c01222f86fdc2c5e55ce0d
王信凱 <jcskyting@gmail.com>
2015-09-13 13:39:48 +0800

    api issue create

--- app/controllers/api_v1/issues_controller.rb
+++ app/controllers/api_v1/issues_controller.rb
@@ -9,5 +9,23 @@ class ApiV1::IssuesController < ApiController
     @celebrates = @issue.vote_users.where(role: 1)
   end
 
+  def create
+    @issue = Issue.new( title: params[:title],
+                        description: params[:discription],
+                        creator: params[:authentication_token]
+      )
+
+    if @issue.save
+      render :json => { :status => 200,
+                        :message => "Issue create ok",
+                        :user => user,
+                        :auth_token => user.authentication_token,
+                        :issue => @issue
+                      }
+
+    else
+      render :json => { :message => "Issue create fault" }, :status => 401
+    end
+  end
 
 end
