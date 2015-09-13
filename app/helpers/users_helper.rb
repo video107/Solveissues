88f16cf919d6f12b0befdd3f3814a1d373193462
王信凱 <jcskyting@gmail.com>
2015-09-13 10:21:39 +0800

    country count

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+
+  def return_total_user_country_hash(total_user)
+    country_hash = Hash.new
+    total_user.uniq.each do |user|
+      User.find_by_email(user).country
+      unless country_hash[User.find_by_email(user).country]
+        country_hash[User.find_by_email(user).country]=1
+      else
+        country_hash[User.find_by_email(user).country]+=1
+      end
+    end
+    country_hash
+    # r_html = ""
+    # country_hash.each_key do |k|
+    #   r_html << %Q(<span class="label label-warning">#{k}:#{country_hash[k]}</span>)
+    # end
+    # r_html
+  end
 end
