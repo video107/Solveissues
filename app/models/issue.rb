d9be6a2672c3fbfaf3480cf9094e1001f75985fa
John Chang <tschangc@gmail.com>
2015-09-13 08:30:57 +0800

    commit

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -4,4 +4,10 @@ class Issue < ActiveRecord::Base
   has_many :votes, :dependent => :destroy
   has_many :vote_users, through: :votes, source: :user, :dependent => :destroy
 
+
+  def find_vote_by_user(user)
+    self.votes.where(user_id: user.id).first
+  end
+
+
 end
