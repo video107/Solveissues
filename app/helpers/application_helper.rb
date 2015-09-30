dac88a6eec704401aea84aa38a560e7fe3cf84ad
王信凱 <jcskyting@gmail.com>
2015-10-01 00:33:21 +0800

    flipy css: rotateY(180deg)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,8 @@ module ApplicationHelper
     "<span class=\"glyphicon glyphicon-#{name}\"><span>".html_safe
   end
 
-  
+  def icon_flipy(name)
+    "<span class=\"glyphicon glyphicon-#{name} flipy\"><span>".html_safe
+  end
+
 end
