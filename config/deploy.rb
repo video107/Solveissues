74007f6d18044b191274380a53ff9a6b69885d27
王信凱 <jcskyting@gmail.com>
2015-11-14 13:35:54 +0800

    deploy.rb setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'solveissues'
+set :application, 'Solveissues'
 set :repo_url, 'git@github.com:jcsky/Solveissues.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deploy/solveissues'
+set :deploy_to, '/home/deploy/Solveissues'
 
 # Default value for :scm is :git
 # set :scm, :git
