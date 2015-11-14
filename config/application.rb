a62fe2b7edefbb90b4559efd7408038ae4572032
王信凱 <jcskyting@gmail.com>
2015-11-14 14:48:16 +0800

    deploy config.eager_load is set to nil error application.rb config.action_dispatch.cookies_serializer = :hybrid

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Solveissues
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.action_dispatch.cookies_serializer = :hybrid
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
