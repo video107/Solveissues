e5a517639add68e8cb6d9d8e0d4cfcdd043eaa0e
王信凱 <jcskyting@gmail.com>
2015-11-18 00:12:47 +0800

    set deploy develop branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 server '139.162.28.217', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-
+set :branch, 'develop'
 
 
 # role-based syntax
